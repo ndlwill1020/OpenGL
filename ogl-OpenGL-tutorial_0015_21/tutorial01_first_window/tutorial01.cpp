e5d09f41204651d5fd8552a71c6a82e7fddf54c5
ndl_mac <ndl_mac1020@126.com>
2016-08-04 15:30:09 +0800

    commit "use cmake convert src to xcodeproj"

---
+++ ogl-OpenGL-tutorial_0015_21/tutorial01_first_window/tutorial01.cpp
@@ -0,0 +1,78 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+//GLFW来帮我们处理窗口、键盘消息等细节,或者用别的库，比如FreeGLUT
+#include <glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+//三维数学库
+#include <glm/glm.hpp>
+using namespace glm;
+
+int main( void )
+{
+	// Initialise GLFW
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+
+	// Open a window and create its OpenGL context
+    //创建我们的第一个OpenGL窗口
+	window = glfwCreateWindow( 1024, 768, "Tutorial 01", NULL, NULL);
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window.\n" );
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+	do{
+		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+		glClear( GL_COLOR_BUFFER_BIT );
+
+		// Draw nothing, see you in tutorial 2 !
+
+		
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
+
